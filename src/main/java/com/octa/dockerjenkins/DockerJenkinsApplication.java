4c7f644057607ac4e348547925bb721dfce94da8
Octaniel <otanieljose30@gmail.com>
2021-09-08 09:40:24 +0000

    tttttttttttt

--- src/main/java/com/octa/dockerjenkins/DockerJenkinsApplication.java
+++ src/main/java/com/octa/dockerjenkins/DockerJenkinsApplication.java
@@ -46,7 +46,7 @@ public class DockerJenkinsApplication {
     }
     @RequestMapping("")
     public String restw(){
-        return "LLLLLLLLLLLLLLLL";
+        return "QQQQQQQQQQQQQQQQ";
     }
 
     @RequestMapping("rr")
